71a4a99a9cd322427b09ad92ff37033bb996ee21
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-06 13:06:04 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Shaurya";
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); //older version
+
+//use backticks bcoz of string interpolation
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('HiteshAmc');
+
+console.log(gameName[0]);
