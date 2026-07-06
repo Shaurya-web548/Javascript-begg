11587fed557f33fc2b495def10e1b8a1b7451250
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-06 11:11:15 +0000

    comaprison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+
+// console.log("2" > 1); //automatically js converts "2" into number.
+
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+/*
+The reason is that an equity check == and comparison > < >=
+<= work differnetly.
+
+Comparisons convert null to a number,treating it as a 0.
+*/
+
+
+// console.log(undefined == 0);
+
+
+// === checks the datatype as well
+
+console.log("2" === 2);
+
+
+
+
