c43cc8bafc7405eb82538cbf3ecbd469222835b8
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-06 10:24:26 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert(3 + 3); // we are using node js not browser.
+
+console.log(3 + 3);
+// code readability should be high
+console.log("Shaurya");
+
+
+
+let name = "Aman";
+let age = 18;
+let isLoggedIn = false;
+let state ;
+//number => 2 to power 53
+
+//bigint => for trading , stock market , companies..
+
+//string => ""
+
+//boolean => true / false
+//null => standalone value
+
+//undefined => 
+
+//symbol => mostly used in react and when we make more components.to find uniqueness
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
