3d7bf695992684e6da85d93cf31fb054c5221b27
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-07 03:10:54 +0000

    nums

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,25 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));//give commas to the number to read the num easily.
+
+//+++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));//Absolute Value
+console.log(Math.round(4.9));//Round Off
+console.log(Math.ceil(4.2));//Make the value +1 if it is .1 more than num.
+
+
+
