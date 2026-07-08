562f52f4b1275cb9ef2851abe42f030cfbb056a4
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-08 14:17:38 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton - when we declare from literals than there is no formation of singleton.When made from constructor than singleton will form.
+// Object.create => constructor
+
+//object literals
+const Jsuser = {
+    name : "Shaurya",
+    age : 18,
+    location : "Jaipur",
+    email : "shaurya@google.com",
+    isLoggedIn : false,
+    lastLoggedINDays : ["Monday" , "Wednesday"]
+}
+
+
+
