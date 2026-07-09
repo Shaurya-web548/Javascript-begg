97b155530e2370094cfef682c3d07f5b79084789
Shaurya-web548 <shauryaagarwal787@gmail.com>
2026-07-09 15:32:00 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,14 +2,37 @@
 // Object.create => constructor
 
 //object literals
+
+const mySym = Symbol("key1");
+
 const Jsuser = {
     name : "Shaurya",
+    "full name" : "Shaurya Agarwal",
+    [mySym] : "mykey1",//imp!!
     age : 18,
     location : "Jaipur",
     email : "shaurya@google.com",
     isLoggedIn : false,
     lastLoggedINDays : ["Monday" , "Wednesday"]
 }
+
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser[mySym]);
+
+Jsuser.email = "aman@gpt.com";
+// Object.freeze(Jsuser)//Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+// console.log(Jsuser);
+
+Jsuser.greeting = function(){
+    console.log("Hello Js User");   
+}
+Jsuser.greetingTwo = function(){
+    console.log(`Hello Js user , ${this.name}`);   
+}
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
+
 
 
 
